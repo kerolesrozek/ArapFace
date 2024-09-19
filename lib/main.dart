e3a2506f237ceaca308e03c259a9d0fcf0746530
Your Name <you@example.com>
2024-09-18 23:09:03 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(ArapFace());
+}
+class ArapFace extends StatelessWidget {
+  const ArapFace({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
