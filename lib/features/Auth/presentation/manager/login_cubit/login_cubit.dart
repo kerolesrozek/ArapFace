3da9544c1b17703a714d2552a518d21e25a8357f
Your Name <you@example.com>
2024-09-19 15:58:14 -0700

    create login cubit

--- lib/features/Auth/presentation/manager/login_cubit/login_cubit.dart
+++ lib/features/Auth/presentation/manager/login_cubit/login_cubit.dart
@@ -1,8 +1,31 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:arapface/features/Auth/domain/entities/person_auth_entity.dart';
+import 'package:arapface/features/Auth/domain/usecases/login_usecase.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'login_state.dart';
 
 class LoginCubit extends Cubit<LoginState> {
-  LoginCubit() : super(LoginInitial());
+  LoginCubit(this.loginUsecase) : super(LoginInitial());
+  final LoginUsecase loginUsecase;
+
+  login({required PersonAuthEntity personAuthEntity}) async {
+    emit(
+      LoginLoading(),
+    );
+    var results = await loginUsecase.call(personAuthEntity: personAuthEntity);
+    results.fold(
+      (fail) {
+        emit(
+          LoginFailure(
+            errorMessage: fail.errorMessage,
+          ),
+        );
+      },
+      (sucess) {
+        emit(
+          LoginSuccess(),
+        );
+      },
+    );
+  }
 }
